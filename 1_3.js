6f0aba461c18d2c65aa33c33ebd3033cd4ea6448
Maxim <maze.zte@gmail.com>
2023-03-16 11:31:52 +1000

    Improve 1_3.js code

--- 1_3.js
+++ 1_3.js
@@ -1,18 +1,18 @@
 function findTwoMaximalNumbers(str) {
-    let max1 = -Infinity;
-    let max2 = -Infinity;
+    let max1 = null;
+    let max2 = null;
     let current = '';
 
     for (let i = 0; i < str.length; i++) {
         const char = str[i];
         if (char === ' ') {
-            current =  current.trim();
-            if (current !== '' && !isNaN(+current) && isFinite(current)) {
-                let num = +current;
-                if (num > max1) {
+            current = current.trim();
+            let num = +current;
+            if (current !== '' && !isNaN(num) && isFinite(num)) {
+                if (max1 === null || num > max1) {
                     max2 = max1;
                     max1 = num;
-                } else if (num > max2) {
+                } else if (max2 === null || num > max2) {
                     max2 = num;
                 }
             }
@@ -24,10 +24,10 @@ function findTwoMaximalNumbers(str) {
 
     const num = Number(current);
     if (!isNaN(num)) {
-        if (num > max1) {
+        if (max1 === null || num > max1) {
             max2 = max1;
             max1 = num;
-        } else if (num > max2) {
+        } else if (max2 === null || num > max2) {
             max2 = num;
         }
     }
@@ -41,14 +41,13 @@ if (str === '') {
     alert('Строка не содержит чисел!');
 } else {
     let maxNumbers = findTwoMaximalNumbers(str);
-    if (maxNumbers[0] !== -Infinity) {
-        if (maxNumbers[1] === -Infinity) {
+    if (maxNumbers[0] !== null) {
+        if (maxNumbers[1] === null) {
             alert(`Максимальное число в строке: ${maxNumbers[0]}`);
         } else {
             alert(`Два максимальных числа в строке: ${maxNumbers[0]} и ${maxNumbers[1]}`);
         }
-    }
-    else {
+    } else {
         alert('Строка не содержит чисел!');
     }
 }
